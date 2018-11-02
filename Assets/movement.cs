4e6d6cb5a8d608c3b4f40ab196b371b6f88d384e
unknown <osherz5@gmail.com>
2018-11-02 18:23:27 +0200

    Some demo

---
+++ Assets/movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+    Vector3 speed;
+    GameObject bulletPref;
+
+	// Use this for initialization
+	void Start () {
+        this.speed = Vector3.zero;
+        bulletPref = Resources.Load<GameObject>("BulletPrefab");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position += speed;
+
+        speed = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        speed *= 0.2f;
+
+        if (Input.GetButton("Fire1"))
+        {
+            GameObject b = Instantiate(bulletPref);
+            b.transform.position = this.transform.position;
+            
+        }
+		
+	}
+}
